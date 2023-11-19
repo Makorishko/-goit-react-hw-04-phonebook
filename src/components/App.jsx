ce5e4d06c32590e584b4023fc89697ef6ba8ec37
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-11-19 21:14:35 +0100

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
+import { ContactForm } from './form/form';
+import { Filter } from './filter';
+import { useState, useEffect } from 'react';
+import { ContactList } from './list/list';
+import { Wrapper } from './wrapper-styled';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filters, setFilters] = useState('');
+
+  const addContact = newContact => {
+    const isSame = contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (isSame) {
+      return alert('This name already exist');
+    }
+
+    setContacts(prevState => [...prevState, newContact]);
+  };
+
+  const changeFilter = evt => {
+    setFilters(evt.target.value);
+  };
+
+  const deleteElementsOfList = contact => {
+    setContacts(contacts.filter(item => item.id !== contact.id));
+  };
+
+  const getFilteredList = () => {
+    if (contacts.length) {
+      return contacts.filter(item =>
+        item.name.toLowerCase().includes(filters.toLowerCase())
+      );
+    } else {
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts && savedContacts.length) {
+      setContacts(() => JSON.parse(savedContacts));
+    }
+  }, []);
+
+  useEffect(
+    (_, prevState) => {
+      if (contacts !== prevState) {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+      }
+    },
+    [contacts]
+  );
+
+  const filteredList = getFilteredList();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter changeFilter={changeFilter} />
+      <ContactList
+        contacts={filteredList}
+        deleteElementsOfList={deleteElementsOfList}
+      />
+    </Wrapper>
   );
 };
